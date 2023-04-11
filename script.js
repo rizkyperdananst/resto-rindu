9c1184714a25256a5c8e8a42301992d6d3754b4a
rizkyperdananst <riskiperdana345@gmail.com>
2023-04-11 11:57:59 +0700

    RR-1/Button-To-Top

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// Menu Toggle
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
      nav.classList.toggle('slide');
+})
+
+// Scroll To Top
+const toTop = document.querySelector(".to-top");
+  
+window.addEventListener("scroll", () => {
+     if (window.pageYOffset > 100) {
+         toTop.classList.add("active");
+     } else {
+         toTop.classList.remove("active");
+     }
 })
